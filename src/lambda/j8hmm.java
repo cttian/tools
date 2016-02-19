ebe4be0bdf417c022f20d0a42be98feeb15728b6
codedayday <codedayday@126.com>
2016-02-19 12:02:04 +0800

    简单前向

--- src/lambda/j8hmm.java
+++ src/lambda/j8hmm.java
@@ -5,15 +5,16 @@ import java.util.List;
 
 public class j8hmm {
 	public static void main(String[] args) {
-		Hmm hmm = new Hmm(new double[]{0.5, 0.5}, new double[][]{{0.5, 0.5}, {0.5, 0.5}}, new double[][]{{0.5, 0.5}, {0.5, 0.5}});
+		Hmm hmm = new Hmm(new double[] { 0.5, 0.5 }, new double[][] { { 0.5, 0.5 }, { 0.5, 0.5 } },
+				new double[][] { { 0.5, 0.5 }, { 0.5, 0.5 } });
 		System.out.println(Forward.cal(hmm, Arrays.asList(0, 1)));
 	}
 }
 
 class Hmm {
 	public double pi[]; // 初始概率
-	public double[][] a; // 隐藏状态转换概率
-	public double[][] b; // 输出概率
+	public double[][] a; // 隐藏状态(0,1)转换概率 
+	public double[][] b; // 输出可能的都是0,1;输出概率
 
 	public Hmm(double[] pi, double[][] a, double[][] b) {
 		this.pi = pi;
@@ -30,7 +31,11 @@ class Hmm {
 class Forward {
 
 	public static double cal(final Hmm hmm, final List<Integer> o) {
-		final Double[][] p = new Double[o.size()][hmm.stateSize()];
+		final Double[][] p = Arrays.asList(new Double[o.size()][]).stream().map(e -> {
+			e = new Double[hmm.stateSize()];
+			Arrays.fill(e, 0.);
+			return e;
+		}).toArray(Double[][]::new);
 		for (int i = 0; i < hmm.stateSize(); i++) {
 			p[0][i] = hmm.pi[i] * hmm.b[i][o.get(i)];
 		}
@@ -41,6 +46,6 @@ class Forward {
 				}
 			}
 		}
-		return Arrays.asList(p).stream().mapToDouble(null).sum();
+		return Arrays.asList(p[o.size() - 1]).stream().mapToDouble(e -> e).sum();
 	}
 }
